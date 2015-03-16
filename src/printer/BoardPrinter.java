0a876c0b0ebb66faa7933919b1ee41a693521d38
ariel <ariel@10.0.100.75>
2015-03-16 15:52:09 +0900

    開発環境の文字コードを修正

--- src/printer/BoardPrinter.java
+++ src/printer/BoardPrinter.java
@@ -8,20 +8,20 @@ public class BoardPrinter {
 	}
 	public void printOneLine(int array[]){
 		this.lineCounter++;
-		System.out.print(lineCounter+":");
+		//System.out.print(lineCounter+":");
 		for(int i=0;i<array.length;i++) {
 			if(i==array.length-1) {
-				System.out.println("("+(i+1)+","+array[i]+")");
+				//System.out.println("("+(i+1)+","+array[i]+")");
 				}
 			else{ 
-				System.out.print("("+(i+1)+","+array[i]+")");
+				//System.out.print("("+(i+1)+","+array[i]+")");
 			}
 		}
 	}
 	
 	public void printFinalLine(){
-		System.out.println("finished");
-		System.out.println("total case is="+lineCounter);
+		//System.out.println("finished");
+		//System.out.println("total case is="+lineCounter);
 	}
 
 }
--- src/productmain/ProductMain.java
+++ src/productmain/ProductMain.java
@@ -7,14 +7,13 @@ import printer.BoardPrinter;
 public class ProductMain {
 
 	public static void main(String[] args) {
-		
 		long start = System.currentTimeMillis();
 		int count =Integer.parseInt(args[0]);
 		ArrayChecker arrayChecker=new ArrayChecker();
 		BoardPrinter boardPrinter=new BoardPrinter();
 		ArrayBuilder arrayBuilder= new ArrayBuilder(count, boardPrinter, arrayChecker);
 		long stop = System.currentTimeMillis(); 
-		System.out.println("Às‚É‚©‚©‚Á‚½ŠÔ‚Í" + (stop - start) + "ƒ~ƒŠ•b‚Å‚·"); 
+		System.out.println("å®Ÿè¡Œã«ã‹ã‹ã£ãŸæ™‚é–“ã¯" + (stop - start) + "ãƒŸãƒªç§’ã§ã™ã€‚"); 
 		
 	}
 }
